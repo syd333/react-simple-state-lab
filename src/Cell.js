0a021ce27a0033ce3b6e994e309e39e89f1e15e7
syd333 <sydmiranda@icloud.com>
2021-03-16 17:00:46 -0700

    commit tree 45376f742d12d2373f79e287a45c8f81e70b060f

---
+++ src/Cell.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Cell extends React.Component {
+    constructor(props){
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    handleClick = () => {
+     this.setState({color: '#333'})
+    // console.log(here)
+}
+
+
+    render(){
+        return( 
+            <div className ="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+            </div>
+        )
+    }
+}
+
+export default Cell;
+
+
+// 1) has an event listener for clicks on the <div> (don't forget: 'onClick' in React!)
+// 2) has an event listener that, when clicked, calls this.setState() once (make sure you aren't setting state directly, but instead using the component's 'setState' method)
+// 3) has an event listener that, when clicked, sets state's 'color' key to a value of '#333'
